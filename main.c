570be8538428f72a091231ef8fa2803df35d8ef4
Boodenator <boode1000@gmail.com>
2024-03-28 07:38:46 -0700

    montyH

--- main.c
+++ main.c
@@ -1,83 +1,33 @@
 #include "monty.h"
-stack_t *head = NULL;
+
+glob_t glob;
 
 /**
- * main - entry point
- * @argc: arguments count
- * @argv: list of arguments
- * Return: always 0
+ * main - entry point for the monty program
+ * @argc: number of cmd line arguments
+ * @argv: array of cmd line argument strings
+ * Return: 0 if success
  */
-
 int main(int argc, char *argv[])
 {
+	stack_t *stack = NULL;
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	open_file(argv[1]);
-	free_nodes();
-	return (0);
-}
 
-/**
- * create_node - Creates a node.
- * @n: Number to go inside the node.
- * Return: Upon sucess a pointer to the node. Otherwise NULL.
- */
-stack_t *create_node(int n)
-{
-	stack_t *node;
-
-	node = malloc(sizeof(stack_t));
-	if (node == NULL)
-		err(4);
-	node->next = NULL;
-	node->prev = NULL;
-	node->n = n;
-	return (node);
-}
-
-/**
- * free_nodes - Frees nodes in the stack.
- */
-void free_nodes(void)
-{
-	stack_t *tmp;
-
-	if (head == NULL)
-		return;
-
-	while (head != NULL)
+	glob.file = fopen(argv[1], "r");
+	if (glob.file == NULL)
 	{
-		tmp = head;
-		head = head->next;
-		free(tmp);
-	}
-}
-
-
-/**
- * add_to_queue - Adds a node to the queue.
- * @new_node: Pointer to the new node.
- * @ln: line number of the opcode.
- */
-void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
-{
-	stack_t *tmp;
-
-	if (new_node == NULL || *new_node == NULL)
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
-	if (head == NULL)
-	{
-		head = *new_node;
-		return;
 	}
-	tmp = head;
-	while (tmp->next != NULL)
-		tmp = tmp->next;
 
-	tmp->next = *new_node;
-	(*new_node)->prev = tmp;
-
+	execute_file(&stack);
+	fclose(glob.file);
+	free(glob.line);
+	free_stack(stack);
+	exit(EXIT_SUCCESS);
 }
