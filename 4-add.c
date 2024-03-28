ec5b95d786a49f35325ec827de89a8a10511da58
Boodenator <boode1000@gmail.com>
2024-03-28 13:26:59 -0700

    task 5

--- 4-add.c
+++ 4-add.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- * f_add - adds the top two elements of the stack.
+ * f_add - adding top two elements in stack.
  * @head: stack head
  * @counter: line_number
  * Return: no return
@@ -19,8 +19,8 @@ void f_add(stack_t **head, unsigned int counter)
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
+		fclose(var.file);
+		free(var.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
