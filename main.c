b73dc4016a7c2690ed3f2dbe908fba0ccffc2f0d
Boodenator <boode1000@gmail.com>
2024-03-28 18:39:31 -0700

    main

--- main.c
+++ main.c
@@ -1,45 +1,83 @@
 #include "monty.h"
+stack_t *head = NULL;
 
-var_t var = {NULL, NULL, NULL, 0};
 /**
-* main - code interpreter
-* @argc: arguments no
-* @argv: file location
-* Return: 0 if success
-*/
+ * main - entry point
+ * @argc: arguments no
+ * @argv: arguments list
+ * Return: always 0
+ */
+
 int main(int argc, char *argv[])
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
-
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	var.file = fopen(argv[1], "r");
-	if (!file)
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
+
+/**
+ * create_node - make new node.
+ * @n: Number to go inside the node.
+ * Return: if sucess ptr to the node. else NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *new_nnode;
+
+	new_nnode = malloc(sizeof(stack_t));
+	if (new_nnode == NULL)
+		err(4);
+	new_nnode->next = NULL;
+	new_nnode->prev = NULL;
+	new_nnode->n = n;
+	return (new_nnode);
+}
+
+/**
+ * free_nodes - Free the nodes at stack.
+ */
+void free_nodes(void)
+{
+	stack_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
+
+/**
+ * add_to_queue - Adds new node to quee.
+ * @new_node: Ptr to new node.
+ * @ln: line number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *temp;
+
+	if (new_node == NULL || *new_node == NULL)
 		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
+	if (head == NULL)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		var.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
+		head = *new_node;
+		return;
 	}
-	free_stack(stack);
-	fclose(file);
-return (0);
+	temp = head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = *new_node;
+	(*new_node)->prev = temp;
+
 }
