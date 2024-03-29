530e726984178d53c0e6d5aba150b3a5a5778c53
Boodenator <boode1000@gmail.com>
2024-03-28 19:52:16 -0700

    betty fx

--- monty.h
+++ monty.h
@@ -20,9 +20,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+  int n;
+  struct stack_s *prev;
+  struct stack_s *next;
 } stack_t;
 
 /**
@@ -35,8 +35,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+  char *opcode;
+  void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 extern stack_t *head;
@@ -47,26 +47,21 @@ void free_nodes(void);
 void stack_print(stack_t **, unsigned int);
 void stack_add(stack_t **, unsigned int);
 void add_to_queue(stack_t **, unsigned int);
-
 void open_file(char *file_name);
 int divide_line(char *buffer, int line_number, int format);
 void read_file(FILE *);
 int len_chars(FILE *);
 void find_func(char *, char *, int, int);
-
 void call_fun(op_func, char *, char *, int, int);
-
 void add_nodes(stack_t **, unsigned int);
 void sub_nodes(stack_t **, unsigned int);
 void div_nodes(stack_t **, unsigned int);
 void mul_nodes(stack_t **, unsigned int);
 void mod_nodes(stack_t **, unsigned int);
-
 void print_top(stack_t **, unsigned int);
 void pop_top(stack_t **, unsigned int);
 void nop(stack_t **, unsigned int);
 void swap_nodes(stack_t **, unsigned int);
-
 void err(int error_code, ...);
 void more_err(int error_code, ...);
 void string_err(int error_code, ...);
